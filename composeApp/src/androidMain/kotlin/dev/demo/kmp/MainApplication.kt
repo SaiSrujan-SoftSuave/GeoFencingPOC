8b359d4836a601e4e83d17d810ba9c15c3f01d7a
saisoftsuave <sai.babu@softsuave.org>
2025-05-07 13:35:26 +0530

    fixed crash issue

--- composeApp/src/androidMain/kotlin/dev/demo/kmp/MainApplication.kt
+++ composeApp/src/androidMain/kotlin/dev/demo/kmp/MainApplication.kt
@@ -1,10 +1,13 @@
 package dev.demo.kmp
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        initKoin()
+        initKoin() {
+            androidContext(this@MainApplication)
+        }
     }
 }
